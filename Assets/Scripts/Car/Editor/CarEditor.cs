baeaca48e9a11129b6f00d0cc542a72c92cd96b0
ltzan87 <100611591+ltzan87@users.noreply.github.com>
2022-06-20 20:44:17 -0300

    add custom editor script

---
+++ Assets/Scripts/Car/Editor/CarEditor.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Car))]
+public class CarEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        Car myTarget = (Car)target;
+
+        myTarget.speed = EditorGUILayout.IntField("Minha Velocidade", myTarget.speed);
+        myTarget.gear = EditorGUILayout.IntField("Marcha", myTarget.gear);
+
+        EditorGUILayout.LabelField("Velocidade Total", myTarget.TotalSpeed.ToString());
+
+        EditorGUILayout.HelpBox("Calcule a velocidade total do carro", MessageType.Info);
+
+        if(myTarget.TotalSpeed > 200)
+        {
+        EditorGUILayout.HelpBox("Velocidade acima do permitido", MessageType.Error);
+        }
+    }
+}
